257aee26a79e6c196e0a422c3c80b476086a4491
oreistake <otakedazo16@icloud.com>
2025-10-20 10:28:44 +0900

    移動処理
    プレイヤーの移動処理+モーション切り替え

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    private Animator _animator;
     public float moveSpeed = 5f;
     private Vector2 moveDirection;
 
     // Start is called before the first frame update
+
+    void Start()
+    {
+
+        // FPSÇ60Ç…ê›íË
+        Application.targetFrameRate = 60;
+
+        
+        _animator = GetComponent<Animator>();
+    }
     void Update()
     {
         ProcessInputs();
@@ -19,10 +30,21 @@ public class PlayerMove : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         moveDirection = new Vector2(moveX, moveY).normalized;
+
+        if (moveX > 0)
+        {
+            transform.eulerAngles = new Vector3(0,0,0);
+        }
+        else if (moveX < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
     }
 
     void Move()
     {
-        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime,Space.World);
+        _animator.SetBool("Walk", moveDirection.x != 0.0f || moveDirection.y != 0.0f);
     }
 }

bef053f6f118dbbbf8e9e031f89b4e8491cf31eb
oreistake <otakedazo16@icloud.com>
2025-11-04 12:01:42 +0900

    敵
    敵のPreFab化、生成

---
+++ Assets/Script/EnemySpawner.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // 敵オブジェクト
+    [SerializeField] private GameObject[] _enemy;
+
+    // 敵を生成する時間
+    [SerializeField] private float[] _spawnTimes;
+
+    [SerializeField]private float _spawnCount;
+    private int _spawnNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spawnCount = 0.0f;
+        _spawnNum = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Spawn();
+    }
+
+    private void _Spawn()
+    {
+        if (_spawnNum > _enemy.Length - 1) return;
+
+        _spawnCount += Time.deltaTime;
+        if(_spawnCount >= _spawnTimes[_spawnNum])
+        {
+            Instantiate(_enemy[_spawnNum]);
+            _spawnNum++;
+            _spawnCount = 0.0f;
+
+        }
+    }
+}

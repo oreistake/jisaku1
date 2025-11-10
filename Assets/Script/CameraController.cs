d0e37cda480e6961433d695c0aa90282598c827a
oreistake <otakedazo16@icloud.com>
2025-11-10 09:36:16 +0900

    カメラ制御
    カメラ制御

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -1,24 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.Tilemaps;
 
 public class CameraController : MonoBehaviour
 {
-    private GameObject player;
+    public GameObject player; // 追従するプレイヤー
+    public Tilemap backgroundTilemap; // Tilemap をインスペクターで指定
 
-    // Start is called before the first frame update
+    private Camera cam;
+    private float minX, maxX, minY, maxY;
+
     void Start()
     {
-        this.player = GameObject.Find("luchador-idle-right_0" +
-            "");
+        cam = Camera.main;
+
+        // Tilemap の範囲を取得
+        Bounds bounds = backgroundTilemap.localBounds;
+        minX = bounds.min.x;
+        maxX = bounds.max.x;
+        minY = bounds.min.y;
+        maxY = bounds.max.y;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        Vector3 playerPos=this.player.transform.position;
-        transform.position = new Vector3(
-            playerPos.x, playerPos.y, transform.position.z);
+        if (player == null || cam == null) return;
+
+        // カメラの表示範囲
+        float camHeight = cam.orthographicSize * 2f;
+        float camWidth = camHeight * cam.aspect;
+
+        // プレイヤー位置をClamp
+        Vector3 playerPos = player.transform.position;
+        float clampX = Mathf.Clamp(playerPos.x, minX + camWidth / 2f, maxX - camWidth / 2f);
+        float clampY = Mathf.Clamp(playerPos.y, minY + camHeight / 2f, maxY - camHeight / 2f);
+
+        // カメラ移動
+        transform.position = new Vector3(clampX, clampY, transform.position.z);
     }
 }

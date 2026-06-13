c54c7d29f9f9f2eb6a135d5d9191baf8e1709eac
oreistake <otakedazo16@icloud.com>
2026-06-13 19:58:57 +0900

    聖書
    聖書の動き方を考える

--- Assets/Script/Weapon/BibleAttack.cs
+++ Assets/Script/Weapon/BibleAttack.cs
@@ -9,22 +9,32 @@ public class BibleAttack : MonoBehaviour
     [SerializeField] private float _radius;
     [SerializeField] private float _speed;
     [SerializeField] private float _angle;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Vector3 _position;
+    [SerializeField] private bool a;
+    private void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        _Bible.transform.position = _Player.transform.position;
-        _angle += _speed * Time.deltaTime * Mathf.PI * 2f;
 
-        float x = Mathf.Cos(_angle) * _radius;
-        float z = Mathf.Sin(_angle) * _radius;
-        transform.position = _Bible.transform.position + new Vector3(x, 0f, z);
+        if(Input.GetKeyDown(KeyCode.Space))a=!a;
+
+        if(a)
+        {
+            _Bible.SetActive(true);
+            _Bible.transform.position = _Player.transform.position;
+            _position= _Bible.transform.position;
+            _angle += _speed * Time.deltaTime * Mathf.PI * 2f;
+
+            float x = Mathf.Cos(_angle) * _radius;
+            float y = Mathf.Sin(_angle) * _radius;
+            _Bible.transform.position = _position + new Vector3(x, y, 0f);
+        }
+        else
+        {
+            _Bible.SetActive(false);
+        }
     }
 }
 
